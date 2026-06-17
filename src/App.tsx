d6a0489bacee7e73aff62f438a6e32d38db8e4a6
Mohd Asad <149827980+mohd-asad17@users.noreply.github.com>
2026-06-17 18:29:35 +0530

    Remove redundant variable assignment in loadGraph

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ export default function App() {
       try {
         setLoading(true);
   
-        const graph = await getAppGraph(appId);
+        await getAppGraph(appId);
   
         setNodes([]);
 setEdges([]);
